9d6b288e117689902484a14c8136220a804582f5
Yaroslav Kuznetsov <yaroslav.kuznetsov@devexpress.com>
2026-04-27 20:21:38 +0400

    add backdrop

--- BlazorCustomization/wwwroot/scripts/fullscreen.js
+++ BlazorCustomization/wwwroot/scripts/fullscreen.js
@@ -1,13 +1,7 @@
 var customApi = window["customApi"] || (window["customApi"] = {});
 
 customApi.requestFullscreen = function (element) {
-    if (element.requestFullscreen) {
-        element.requestFullscreen();
-    } else if (element.webkitRequestFullscreen) {
-        element.webkitRequestFullscreen();
-    } else if (element.mozRequestFullScreen) {
-        element.mozRequestFullScreen();
-    } else if (element.msRequestFullscreen) {
-        element.msRequestFullscreen();
+    if (window.document.body.requestFullscreen) {
+        window.document.body.requestFullscreen();
     }
 }
